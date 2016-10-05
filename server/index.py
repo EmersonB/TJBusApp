faefdaf8bd1855ee0f1dbf07d923f9baa1136db5
Kevin Geng <gengkev@gmail.com>
2016-10-05 13:50:12 -0400

    Try to add MySQL stuff

--- server/index.py
+++ server/index.py
@@ -1,35 +1,70 @@
 # flask server to create rest api
 
 from flask import Flask, request, send_from_directory
+from flask.ext.mysql import MySQL
+
 
 PORT = 4000
 
-app = Flask(__name__,static_url_path='')
 
-@app.route("/", methods=['GET'])
+mysql = MySQL()
+app = Flask(__name__)
+app.config['MYSQL_DATABASE_USER'] = 'username'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
+app.config['MYSQL_DATABASE_DB'] = 'test'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+
+@app.route('/', methods=['GET'])
 def home():
-	return send_from_directory("static","index.html")
+    return send_static_file('index.html')
 
-@app.route("/post",methods=['GET'])
-def post():
-	busNumber = request.args.get('busNumber')
-	x = request.args.get('x')
-	y = request.args.get('y')
-	if ( False ): # check if request is valid here
-		return "invalid", 400
-	# make some sql request here on the data
-	return "success" , 200
 
-@app.route("/bus/<string:bus_id>",methods=['GET'])
-def getBus():
-	if ( False ): #check if valid bus id
-		return "invalid", 400 
-	# make sql request here
-	return { "some" : "json" }, 200
+@app.route('/bus/<int:bus_id>', methods=['GET', 'POST'])
+def update_bus(bus_id=None):
+    if not bus_id:
+        return {'error': 'Please specify a bus_id.'}, 400
 
-@app.route("/bus",methods=['GET']) # gets all buses
-def getBuses():
-	return { "some" : "json", "with lots of" : "buses" }, 200
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    if request.method == 'POST':
+        # TODO: check authentication here
+        date = request.form['date']
+        position_id = request.form['position_id']
+        last_updated = datetime.datetime.now()
+
+        sql = 'INSERT INTO `assignment` (date, bus_id, position_id, last_updated) (%s, %s, %s, %s'
+        cursor.execute(sql, (date, bus_id, position_id, last_updated))
+        conn.commit()
+        conn.close()
+        return {}, 200
+
+    else:
+        date = request.form['date']  # TODO: fix
+        sql = 'SELECT * FROM `assignment` WHERE `bus_id`=%s AND `date`=%s'
+        cursor.execute(sql, (bus_id, date))
+        result = cursor.fetchone()
+        conn.close()
+        if result is None:
+            return {'error': 'bus_id does not exist'}, 404
+        else:
+            return result, 200
+
+
+@app.route('/bus', methods=['GET']) # gets all buses
+def get_bus_list():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    date = request.form['date']
+    sql = 'SELECT * FROM `assignment` WHERE `date`=%s'
+    cursor.execute(sql, (date,)
+    result = cursor.fetchall()
+    conn.close()
+    return result, 200
+
 
 if __name__ == "__main__":
-	app.run(port=PORT)
+    app.run(port=PORT)
+
