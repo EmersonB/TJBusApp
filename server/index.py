d1e0d556b71e7b2817b458cc33bf8b93b81c4af9
Alex Wang <alex3763@gmail.com>
2016-10-05 11:41:14 -0400

    basic api - returns one value yay

--- server/index.py
+++ server/index.py
@@ -1,1 +1,18 @@
-# will hold flask server, placeholder for now :^)
+# flask server to create rest api
+
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+PORT = 4000
+
+app = Flask(__name__,static_url_path='')
+api = Api(app)
+
+class TestApi(Resource):
+	def get(self):
+		return "It works!"
+
+api.add_resource(TestApi,"/")
+
+if __name__ == "__main__":
+	app.run(port=PORT)
