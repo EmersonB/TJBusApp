8ca2b0d3176bf2629d5cfad3307b778d0f6c8233
Alex Wang <alex3763@gmail.com>
2016-10-05 20:09:15 -0400

    sql script

---
+++ server/setup.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE `buses`;
+
+USE `buses`;
+
+CREATE TABLE `position`
+(
+	`position_id` INT NOT NULL AUTOINCREMENT,
+	`map_coordinates` TEXT,
+	`angle` DOUBLE,
+	`last_updated` DATETIME,
+	PRIMARY KEY (`position_id`)
+);
+
+CREATE TABLE `bus`
+(
+	`bus_id` INT NOT NULL AUTOINCREMENT,
+	`name` VARCHAR(40),
+	PRIMARY KEY (`bus_id`)
+);
+
+CREATE TABLE `assignment` (
+	`assignment_id` INT NOT NULL AUTOINCREMENT,
+	`date` DATE NOT NULL,
+	`bus_id` INT NOT NULL,
+	`position_id` INT NOT NULL,
+	`last_updated` DATETIME,
+	PRIMARY KEY (`assignment_id`),
+	FOREIGN KEY (`bus_id`) REFERENCES `bus` (`bus_id`),
+	FOREIGN KEY (`position_id`) REFERENCES `position` (`position_id`)
+);
