30721cb438109d4bff5889f5f818eb54a41630a7
Kevin Geng <2017kgeng@tjhsst.edu>
2016-10-14 11:59:14 -0400

    fix stuff

--- server/index.py
+++ server/index.py
@@ -1,6 +1,8 @@
 # flask server to create rest api
 
 import os
+import datetime
+
 from flask import Flask, request, jsonify
 from playhouse.shortcuts import model_to_dict, dict_to_model
 
@@ -24,19 +26,18 @@ def image():
     return app.send_static_file('image.png')
 
 
-@app.route('/assignment/<string:bus_name>', methods=['GET', 'POST'])
-def update_assignment(bus_name=None):
-    if not bus_name:
-        return {'error': 'Please specify a bus_name.'}, 400
+@app.route('/assignment/<string:date>/<string:bus_id>', methods=['GET', 'POST'])
+def update_assignment(date, bus_id=None):
+    if not bus_id:
+        return jsonify(success=False, error='Please specify a bus_id.'), 400
 
     if request.method == 'POST':
         # TODO: check authentication here
-        date = request.form['date']
         position_id = request.form['position_id']
 
         obj = Assignment(
             date=date,
-            name=bus_name,
+            bus=bus_id,
             position=position_id,
             last_updated=datetime.datetime.now())
         obj.save()
@@ -44,22 +45,16 @@ def update_assignment(bus_name=None):
         return jsonify(success=True), 200
 
     else:
-        # TODO also auth here - security concern
-        date = request.form['date']  # TODO: fix
-
         result = Assignment.get(Assignment.bus == bus_id and Assignment.date == date)
         if result is None:
-            return {'error': 'bus_id does not exist'}, 404
+            return jsonify(success=False, error='bus_id does not exist'), 404
         else:
             return jsonify(success=True, result=model_to_dict(result)), 200
 
 
 # get all bus assignments
-@app.route('/assignment', methods=['GET'])
-def get_assignment_list():
-    # TODO auth here
-    date = request.form['date']
-
+@app.route('/assignment/<string:date>', methods=['GET'])
+def get_assignment_list(date):
     query = Assignment.select().where(Assignment.date == date)
     result = list(map(model_to_dict, query))
     return jsonify(success=True, result=result), 200
@@ -68,6 +63,7 @@ def get_assignment_list():
 @app.route('/bus', methods=['GET', 'POST'])
 def get_bus_list():
     if request.method == 'POST':
+        # TODO check auth!!!
         bus_name = request.form['bus_name']
         obj = Bus(name=bus_name)
         obj.save()
@@ -75,7 +71,6 @@ def get_bus_list():
             
     else:
         query = Bus.select()
-        print(query)
         result = list(map(model_to_dict, query))
         return jsonify(success=True, result=result), 200
 
